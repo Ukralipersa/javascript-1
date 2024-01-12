dc11d3e0e71b0a4076319dc0a56bf3dcafc2c9b6
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-12 12:26:25 +0300

    8 - Упражнение "Расчет проекта"

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-let a = 5;
-let b = 5.6;
-console.log(typeof a);
-console.log(typeof b);
-a = 'строка';
-console.log(typeof a);
-let isAdmin = a > 10;
-console.log(typeof isAdmin);
+/*
+Ваша часовая ставка 80$ и вы готовы работать не
+более 5 часов в день 5 дней в неделю (кроме выходных).
+К вам приходит заказчик и предлагает заказ на 40
+часов работы.
+Сейчас понедельник.
+Вы должны уехать через 11 дней.
+Выведете в консоль:
+- Boolean переменную успеете ли вы взяться за работу
+- Сколько вы за неё попросите?
+*/
 
-let c = undefined;
-console.log(typeof c);
-c = 5;
-console.log(typeof c);
+//Данные
+const pricePerHour = 80; //dollars
+const hoursPerDay = 5; // hours
+const Orderhours = 40; // hours
+const iHaveDays = 11; //days
+const iHaveWeekend = 2; //days
 
-let d = null;
-console.log(d == null);
+//Расчет
+const iHaveWorkDay = iHaveDays - iHaveWeekend;
+const iHaveHours = iHaveWorkDay * hoursPerDay;
+const isAccepted = iHaveHours > Orderhours;
+const resultPriceOrder = Orderhours * pricePerHour;
+
+//Результат
+console.log('isAccepted:', isAccepted);
+console.log('resultPriceOrder:', resultPriceOrder);
