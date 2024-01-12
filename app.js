ef8de3fb938be0fad7fe1d57f53bd9fbe671e229
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-12 11:33:15 +0300

    7 - Типы данных

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const isSuited = 100 - 10 > 90 - 5;
-console.log('isSuited:', isSuited);
+let a = 5;
+let b = 5.6;
+console.log(typeof a);
+console.log(typeof b);
+a = 'строка';
+console.log(typeof a);
+let isAdmin = a > 10;
+console.log(typeof isAdmin);
 
-//100 - (12) 10 > (10) 90 - (12) 5
+let c = undefined;
+console.log(typeof c);
+c = 5;
+console.log(typeof c);
 
-const a = (5 + 10) / 2;
-console.log('a:', a);
-
-let b;
-let c;
-c = b = 100 + 50 + 30;
-console.log('c:', c);
-console.log('b:', b);
+let d = null;
+console.log(d == null);
