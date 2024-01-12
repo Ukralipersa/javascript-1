38324075d1e816dd7b7f9af7ca55ac97fdbcc907
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-12 15:14:28 +0300

    13 - Упражнение "Размещение депозита"

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const money = 100;
+/*
+Вася положил 12 000$ на вклад 7% годовых с капитализацией 1 раз в месяц.
+Вывести в консоль, сможет ли он купить дом за 13 500$ через 2 года после снятия вклада. И остаток после покупки.
+Итог - Сумма * (1 + Ставка в месяц не в %) ^ срок в месяцах
+*/
 
-if (money > 50) {
-	console.log('Может купить наш продукт');
-} else if (money > 50) {
-	console.log('Куплен mini продукт');
+//Данные
+const deposit = 12000;
+const yearPercent = 0.07;
+const periodOfDeposit = 24;
+const priceHouse = 13500;
+const monthOfYear = 12;
+
+//Расчет
+const monthPercent = yearPercent / monthOfYear;
+const income = deposit * (1 + monthPercent) ** periodOfDeposit;
+
+if (priceHouse < income) {
+	console.log(`Доход ${income}.
+Вася сможет купить дом за ${priceHouse}$ через 2 года после снятия вклада.
+Остаток ${income - priceHouse}`);
 } else {
-	console.log('Не хватает баланса');
+	console.log(`Доход ${income}.
+Вася не сможет купить дом за ${priceHouse}$ через 2 года после снятия вклада`);
 }
-
-console.log('Итог');
