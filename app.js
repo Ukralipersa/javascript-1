697e8cc6a85b34bef7ffb3c813fe72854c85c19c
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-13 22:25:10 +0300

    35 - Упражнение "Обновление списка задач"

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-const roles = ['user', 'admin', 'manager', 'superuser'];
+/*
+Дан список задач
+const tasks = ['Задача 1'];
+Сделать функции:
+- Добавление задачи в конец
+- Удаление задачи по названию
+- Перенос задачи в начало списка по названию
+Всегда меняем исходный массив
+ */
 
-const url = 'auth/user/login';
-const res = url.split('/');
-console.log(res);
+const tasks = ['Задача 1'];
 
-console.log(roles.join('-'));
+function addTaskToEnd(task) {
+	tasks.push(task);
+}
+
+function deleteTask(task) {
+	const itemIndex = tasks.indexOf(task);
+	if (itemIndex >= 0) {
+		tasks.splice(itemIndex, 1);
+	} else return 'Такой задачи в списке нет';
+}
+
+function transferTaskToStart(task) {
+	const itemIndex = tasks.indexOf(task);
+	if (itemIndex >= 0) {
+		tasks.splice(itemIndex, 1);
+		tasks.unshift(task);
+	} else return 'Такой задачи в списке нет';
+}
+
+addTaskToEnd('Задача 2');
+addTaskToEnd('Задача 3');
+console.log(tasks);
+deleteTask('Задача 2');
+console.log(tasks);
+transferTaskToStart('Задача 3');
+console.log(tasks);
