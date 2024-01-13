56c78a0df9770b651da8cfe73615fcafe7cbb1a0
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-13 13:22:34 +0300

    22 - Введение в функции

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-/*
-Пользователь хочет приобрести игру в магазине
-Он может это сделать только если:
-- Его баланс больше 1000 (balance)
-или число бонусов больше 100 (bonusBalance)
-- Он не забанен (isBanned)
-- Игра не куплена (isExist)
-- Игра в продаже (isSelling)
-Напишите условие для покупки и выведите в консоль
-результат
- */
-
-const balance = 900;
-const bonus = 150;
-const isBanned = false;
-const isExist = false;
-const isSelling = true;
-
-if ((balance > 1000 || bonus > 100) && !isBanned && !isExist && isSelling) {
-	console.log(`Пользователь может купить игру`);
-} else {
-	console.log(`Пользователь не может купить игру`);
+function logName(name, surname) {
+	console.log(`Мое имя ${name} ${surname}`);
 }
+
+logName('Иван', 'Сидоров');
+
+function countDepositSum(depositInUSD, month, rate) {
+	return depositInUSD * (1 + rate / 12) ** month;
+}
+
+const example1 = countDepositSum(1000, 24, 0.12);
+console.log(example1);
+
+console.log(countDepositSum(1000, 48, 0.1));
