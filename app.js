4c03713198d1fbdd1df0deda91f0cc52bdecd29c
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-13 10:59:20 +0300

    18 - Логические операторы

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-/*
-Методом prompt получите ответ пользователя
-на вопрос "Сколько будет 7 + или - 15?". Если ответ верен
-выведите в консоли "Успех", если нет - "Вы робот",
-а если он введет "Я не робот", то тоже "Успех".
-*/
+const isAdmin = true;
+const canWrite = true;
 
-const userAnswer = prompt('Сколько будет 7 + или - 15');
+console.log(`Системный файл ${isAdmin && canWrite}`);
+console.log(`Обычный файл ${isAdmin || canWrite}`);
+console.log(`Инвертируем права админа ${!isAdmin}`);
 
-switch (userAnswer) {
-	case '22':
-	case '-8':
-	case 'Я не робот':
-		console.log('Успех');
-		break;
-	default:
-		console.log('Вы робот');
-}
+const isEdited = true;
+const isSuperAdmin = true;
+
+console.log(`Системный файл с редактированием ${isAdmin && canWrite && (!isEdited || isSuperAdmin)}`);
