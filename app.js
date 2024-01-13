cf6a65e09a6b2b449be7261dddb9360dee1a2cdc
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-13 12:43:39 +0300

    21 - Упражнение "Проверка прав"

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-let age = 0;
-console.log(age || 18);
-console.log(age ?? 18); //возвращает первый аргумент, если он не null/undefined, иначе второй.
+/*
+Пользователь хочет приобрести игру в магазине
+Он может это сделать только если:
+- Его баланс больше 1000 (balance)
+или число бонусов больше 100 (bonusBalance)
+- Он не забанен (isBanned)
+- Игра не куплена (isExist)
+- Игра в продаже (isSelling)
+Напишите условие для покупки и выведите в консоль
+результат
+ */
+
+const balance = 900;
+const bonus = 150;
+const isBanned = false;
+const isExist = false;
+const isSelling = true;
+
+if ((balance > 1000 || bonus > 100) && !isBanned && !isExist && isSelling) {
+	console.log(`Пользователь может купить игру`);
+} else {
+	console.log(`Пользователь не может купить игру`);
+}
