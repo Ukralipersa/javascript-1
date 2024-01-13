d24a5c32418045fab481314ae6252661d8eaf4fb
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-13 16:30:02 +0300

    28 - Функции в функциях

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-function canAccessWebsite(age) {
-	if (age < 18) {
-		return 'Нет'; //Прекращает выполнение после return!
-	}
-	return 'Да';
+const KG_IN_USD = 7;
+const KM_IN_USD = 5;
+
+function calculateW(present) {
+	return present * KG_IN_USD;
 }
 
-console.log(canAccessWebsite(19));
+function calculateKm(distance) {
+	return distance * KM_IN_USD;
+}
 
-const canAccessWebsite2 = age => (age < 18 ? 'Нет' : 'Да');
+function getExchangePrice(present1, present2, distance) {
+	const price1 = present1 * KG_IN_USD;
+	const price2 = present2 * KG_IN_USD;
+	const distancePrice = distance * KM_IN_USD;
+	return price1 + price2 + distancePrice;
+}
 
-console.log(canAccessWebsite2(19));
+function getExchangePrice2(present1, present2, distance) {
+	const price1 = calculateW(present1);
+	const price2 = calculateW(present2);
+	const distancePrice = calculateKm(distance);
+	return price1 + price2 + distancePrice;
+}
+
+console.log(getExchangePrice(1, 2, 10));
+console.log(getExchangePrice2(1, 2, 10));
