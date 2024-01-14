04a861f8121d0a784590d413824fb895ba1a7efd
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-14 17:56:46 +0300

    47 - Callback

--- app.js
+++ app.js
@@ -1,54 +1,25 @@
-/*
-Есть выгрузка операций пользователя
-const operations = [1000, -700, 300, -500, 10000];
-а так же начальный баланс в 100$
-Необходимо сделать функции расчета:
-- Итогового баланса
-- Наличия отрицательного баланса (если после очередной операции
-		баланс < 0, то выдавать false)
-- Расчета среднего расхода и среднего дохода
-*/
-const operations = [1000, -700, 300, -500, 10000];
+function add(a, b) {
+	return a + b;
+}
 
-function closeBalance(operations, startMoney) {
-	let finishMoney = startMoney;
-	for (let element of operations) {
-		finishMoney += element;
-		if (finishMoney < 0) {
-			return false;
-		}
-	}
-	return finishMoney;
+function subtract(a, b) {
+	return a - b;
 }
-// console.log(closeBalance(operations));
 
-function averageCost(operations) {
-	let negative = 0;
-	let count = 0;
-	let res;
-	for (let i = 0; i < operations.length; i++) {
-		if (operations[i] < 0) {
-			negative += operations[i];
-			count++;
-		}
-	}
-	if (count > 0) {
-		return (res = negative / count);
-	}
+function power(a, b) {
+	return a ** b;
 }
-// console.log(averageCost(operations));
-function averageIncome(operations) {
-	let positive = 0;
-	let count = 0;
-	let res;
-	for (let i = 0; i < operations.length; i++) {
-		if (operations[i] > 0) {
-			positive += operations[i];
-			count++;
-		}
-	}
-	if (count > 0) {
-		return (res = positive / count);
-	}
+
+//Функция высшего порядка
+function calculate(a, b, fn) {
+	console.log(fn.name);
+	const res = fn(a, b);
+	return res;
 }
-console.log(averageIncome(operations));
+
+let res = calculate(3, 5, add);
+console.log(res);
+res = calculate(3, 5, subtract);
+console.log(res);
+res = calculate(3, 5, power);
+console.log(res);
