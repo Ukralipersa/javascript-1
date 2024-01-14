a009e12fa138c62cad3da15312c25a0ff05c9b07
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-14 16:48:27 +0300

    46 - Упражнение "Расчет итогового баланса"

--- app.js
+++ app.js
@@ -1,13 +1,54 @@
-const arr = [1, 4, 8, 7, 'str'];
+/*
+Есть выгрузка операций пользователя
+const operations = [1000, -700, 300, -500, 10000];
+а так же начальный баланс в 100$
+Необходимо сделать функции расчета:
+- Итогового баланса
+- Наличия отрицательного баланса (если после очередной операции
+		баланс < 0, то выдавать false)
+- Расчета среднего расхода и среднего дохода
+*/
+const operations = [1000, -700, 300, -500, 10000];
 
-for (let i = 0; i < arr.length; i++) {
-	console.log(arr[i]);
+function closeBalance(operations, startMoney) {
+	let finishMoney = startMoney;
+	for (let element of operations) {
+		finishMoney += element;
+		if (finishMoney < 0) {
+			return false;
+		}
+	}
+	return finishMoney;
 }
+// console.log(closeBalance(operations));
 
-for (let element of arr) {
-	console.log(element);
+function averageCost(operations) {
+	let negative = 0;
+	let count = 0;
+	let res;
+	for (let i = 0; i < operations.length; i++) {
+		if (operations[i] < 0) {
+			negative += operations[i];
+			count++;
+		}
+	}
+	if (count > 0) {
+		return (res = negative / count);
+	}
 }
-
-for (let index in arr) {
-	console.log(arr[index]);
+// console.log(averageCost(operations));
+function averageIncome(operations) {
+	let positive = 0;
+	let count = 0;
+	let res;
+	for (let i = 0; i < operations.length; i++) {
+		if (operations[i] > 0) {
+			positive += operations[i];
+			count++;
+		}
+	}
+	if (count > 0) {
+		return (res = positive / count);
+	}
 }
+console.log(averageIncome(operations));
