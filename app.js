b98c68c55653bf99b36810a943eefc1713a4ff1b
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-14 13:21:56 +0300

    40 - Цикл for

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-/*
-Дан произвольный url - 
-'https://purpleschool.ru/course/javascript'
-Нужно сделать функцию, которая выводит в консоль:
-- Протокол (https)
-- Доменное имя (purpleschool.ru)
-- Путь внутри сайта (/course/javascript)
-*/
-const url = 'https://purpleschool.ru/course/javascript';
+// console.log('Наш баланс 1$');
+// console.log('Наш баланс 2$');
+// console.log('Наш баланс 3$');
+// console.log('Наш баланс 4$');
+// console.log('Наш баланс 5$');
+// console.log('Наш баланс 6$');
+// console.log('Наш баланс 7$');
+// console.log('Наш баланс 8$');
+// console.log('Наш баланс 9$');
 
-function getUrlParts(string) {
-	const protocol = string.split('://', 1);
-	if (protocol[0] === 'https' || protocol === 'http') {
-		const [, , domen, ...others] = string.split('/');
-		others[0] = '/' + others[0];
-		const path = others.join('/');
-		console.log(`Протокол (${protocol[0]})`);
-		console.log(`Доменное имя (${domen})`);
-		console.log(`Путь внутри сайта (${path})`);
-	}
+for (let i = 1; i < 10; i++) {
+	console.log(`Наш баланс ${i}$`);
 }
-
-getUrlParts(url);
