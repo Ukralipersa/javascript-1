b65dd950d27ee74c2afe5a338f9349a9360d92a2
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-17 22:11:51 +0300

    62 - Преобразование строки

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-/*Вытащить имя и фамилию в отдельные переменные */
-const userName = 'Вася aka Terminator Пупкин';
-const firstName = userName.slice(0, userName.indexOf(' '));
-console.log('firstName:', firstName);
-const lastName = userName.slice(userName.lastIndexOf(' ') + 1);
-console.log('lastName:', lastName);
+const str = 'Вася Пупкина';
+console.log(str.includes('а'));
+console.log(str.startsWith('В'));
+console.log(str.endsWith('н'));
+console.log(new String('Вася Пупкин'));
+
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.replace('В', 'Ф'));
+console.log(str.replace('П', 'Д'));
+console.log(str.replace('а', 'и'));
+console.log(str.replaceAll('а', 'и'));
+console.log(str.replace(/а/g, 'и'));
+
+const str2 = ' Вася Пупкина   \n';
+console.log(str2.trimStart());
