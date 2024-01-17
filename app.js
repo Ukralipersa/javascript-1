1331b18dbb7c31232327742a7af88e05f7e61593
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-17 23:57:18 +0300

    63 - Упражнение "Проверка номера"

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const str = 'Вася Пупкина';
-console.log(str.includes('а'));
-console.log(str.startsWith('В'));
-console.log(str.endsWith('н'));
-console.log(new String('Вася Пупкин'));
+/* проверить является ли это номером России */
+/* верные */
+const num1 = '89103235356';
+const num2 = '+79103235356';
+const num3 = '+7(910)3235356';
+const num4 = '+7(910) 323-53-56';
+const num5 = '+7(910) 323-53-56   ';
+/* неверные */
+const num1Error = '89103235';
+const num2Error = '+7d910d323-53-56';
+const num3Error = '9+7103235356';
+const num4Error = '89103g35356';
 
-console.log(str.toLowerCase());
-console.log(str.toUpperCase());
-console.log(str.replace('В', 'Ф'));
-console.log(str.replace('П', 'Д'));
-console.log(str.replace('а', 'и'));
-console.log(str.replaceAll('а', 'и'));
-console.log(str.replace(/а/g, 'и'));
+function isPhoneNumber(num) {
+	let numArray = num.split('');
+	let cleanNum = [];
+	for (let i = 0; i < numArray.length; i++) {
+		if (!isNaN(Number(numArray[i])) && numArray[i] !== ' ') {
+			cleanNum.push(numArray[i]);
+		}
+	}
+	let numString = cleanNum.join('');
+	if ((numString.slice(0, 4) === '8910' || numString.slice(0, 4) === '7910') && numString.slice(4).length === 7) {
+		return true;
+	} else return false;
+}
 
-const str2 = ' Вася Пупкина   \n';
-console.log(str2.trimStart());
+console.log(isPhoneNumber(num5));
+console.log(isPhoneNumber(num4Error));
