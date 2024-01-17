0f5ee6a199b2f492f867cbb94c8edd46778d2727
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-17 21:57:56 +0300

    61 - Упражнение "Выделение имени"

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-const userName = 'Вася Пупкинa';
-console.log(userName);
-console.log(userName[0] + userName[1]);
-console.log(userName.charAt(2));
-
-console.log(userName.length);
-console.log(userName.indexOf('уп'));
-console.log(userName.lastIndexOf('a'));
-console.log(userName.includes('уп'));
-console.log(userName.includes('упс'));
-
-console.log(userName.slice(4));
-console.log(userName.slice(5, 8));
+/*Вытащить имя и фамилию в отдельные переменные */
+const userName = 'Вася aka Terminator Пупкин';
+const firstName = userName.slice(0, userName.indexOf(' '));
+console.log('firstName:', firstName);
+const lastName = userName.slice(userName.lastIndexOf(' ') + 1);
+console.log('lastName:', lastName);
