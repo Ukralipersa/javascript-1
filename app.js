8220286f6d9b2d1a0c106c6850a33d93db3e9c4d
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-19 00:20:39 +0300

    67 - Знакомство с объектами

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-/* Замаскировать все, кроме последних 4х символов */
-let card = '2342834593458353';
-/* ************8353 */
-
-console.log(card.length);
-
-function hideCard(card) {
-	return card.slice(-4).padStart(16, '*');
-}
-
-console.log(hideCard(card));
+const urerArray = ['Вася', 'Пупкин', 24];
+const urer = {
+	name: 'Вася',
+	surname: 'Пупкин',
+	age: 24,
+	skill: ['Программирование', 'Готовка'],
+	1: 1,
+};
