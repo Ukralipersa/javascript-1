d3538a258e2025b83375069b8f1454760dd2c6cd
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-18 23:16:31 +0300

    66 - Упражнение "Маскировка карты"

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const film = 'Звездные войны';
-console.log(film.padStart(50, '*'));
-console.log(film.padEnd(50, '*'));
-console.log(film.repeat(10));
+/* Замаскировать все, кроме последних 4х символов */
+let card = '2342834593458353';
+/* ************8353 */
+
+console.log(card.length);
+
+function hideCard(card) {
+	return card.slice(-4).padStart(16, '*');
+}
+
+console.log(hideCard(card));
