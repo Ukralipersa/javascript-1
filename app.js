99d03c705c0e9521a814b41054616eae6d69ecb4
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-19 02:16:31 +0300

    69 - Упражнение  "Cортировка пользователей"

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-const user = {
-	name: 'Вася',
-	surname: 'Пупкин',
-	age: 24,
-	skills: ['Программирование', 'Готовка'],
-	1: 1,
-	educBasic: 'Школа 10',
-	eduPro: 'МФТИ',
-};
+/* отсортировать пользователей по возрасту */
+const users = [
+	{ name: 'Вася', age: 30 },
+	{ name: 'Катя', age: 18 },
+	{ name: 'Аня', age: 40 },
+	{ name: 'Петя', age: 25 },
+];
 
-console.log(user.city);
-console.log(user.skills);
-console.log(user['skills']);
-const level = 'Pro';
-console.log(user['edu' + level]);
-
-// const res = prompt('Введите свойство');
-user.city = 'Москва';
-user['city'] = 'Москва';
-console.log(user.city);
-
-user.age = 30;
-user['age'] = 30;
-console.log(user.age);
+console.log(users.sort((a, b) => a.age - b.age));
