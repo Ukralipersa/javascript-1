2b6d8d5dc0eecba47d5c762f5ea4facb3dc57990
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-19 23:03:21 +0300

    72 - Упражнение "Кошелек"

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-const user = {
-	name: 'Вася',
-	surname: 'Пупкин',
-	age: 24,
-	getFullName: function () {
-		return this.name + ' ' + this.surname;
+/*
+Реализовать методы увеличения и уменьшения баланса,
+при которых каждая операция сохраняется в массив
+operations в виде { reason: 'Оплата налогов', sum: -100 }.
+Возвращается true, если успешно и false, если не хватает баланса.
+Так же реализовать метод вызова числа операций по кошельку
+*/
+
+const wallet = {
+	balance: 0,
+	operations: [],
+	plusBalance: function (reason, sum) {
+		this.balance += sum;
+		this.operations.push({ reason: reason, sum: sum });
+		return true;
+	},
+	minusBalance: function (reason, sum) {
+		if (sum > this.balance) {
+			console.log('Недостаточно баланса');
+			return false;
+		} else {
+			this.balance -= sum;
+			this.operations.push({ reason: reason, sum: -sum });
+			return true;
+		}
+	},
+	countOperations: function () {
+		return `Число операций кошелька: ${this.operations.length}`;
 	},
 };
 
-const getFullName = function (user) {
-	return user.name + ' ' + user.surname;
-};
+console.log(wallet.plusBalance('Зарплата', 500));
+console.log(wallet.plusBalance('Премия', 100));
 
-console.log(user.getFullName());
+console.log(wallet.minusBalance('Коммунальные услуги', 50));
+console.log(wallet.minusBalance('Покупка продуктов', 150));
+console.log(wallet.minusBalance('Покупка iphone', 1000));
+
+console.log(wallet);
+console.log(wallet.countOperations());
