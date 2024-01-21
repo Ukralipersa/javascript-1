6f82a06fddbd14eda6c4ffb971f9c6272f186b90
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-21 17:01:34 +0300

    89 - Call, apply

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 'use strict';
 
-const b = 1;
-const a = {
-	b,
-	getB: function () {
-		return this.b;
-	},
-	getBAlt() {
-		return this.b;
+const audi = {
+	make: 'audi',
+	model: 'A3',
+	year: 2021,
+	damages: [],
+	addDamage(part, rate) {
+		console.log(`У авто ${this.make} ${this.model} ${this.year} добавлено: повреждение
+${part} со степенью ${rate}`);
+		this.damages.push({
+			part,
+			rate,
+		});
 	},
 };
 
-console.log(a.getBAlt());
+//audi.addDamage('Капот', 1);
+
+const bmw = {
+	make: 'BMW',
+	model: 'X5',
+	year: 2022,
+	damages: [],
+};
+
+bmw.addDamage = audi.addDamage;
+//bmw.addDamage('Бампер', 2);
+
+const addDamageFunc = audi.addDamage;
+//addDamageFunc('Бампер', 2);
+addDamageFunc.call(bmw, ...['Бампер', 2]);
+addDamageFunc.call(audi, 'Бампер', 2);
+
+addDamageFunc.apply(bmw, ['Бампер', 2]);
+addDamageFunc.apply(audi, ['Бампер', 2]);
