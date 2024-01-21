df1572f30b48095ba36af0574b04e681a79bdf43
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-21 17:52:29 +0300

    92 - IIFE

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 'use strict';
-
-/* 
-Создайте объект пользователя с паролем.
-С помощью функции ниже удалить пароль сделав
-функцию сброса пароля
-*/
-
-function removePassword(reset) {
-	if (reset) {
-		this.password = undefined;
-	} else {
-		this.password = '1';
-	}
+//IIFE
+function init() {
+	console.log('Start');
 }
+init();
 
-const user = {
-	name: 'Kate',
-	password: 123456,
-};
+(function () {
+	console.log('Start IIF0');
+	const a = 1;
+})();
 
-const removePasswordForUser = removePassword.bind(user);
-removePasswordForUser(true);
-console.log(user);
+console.log(a);
