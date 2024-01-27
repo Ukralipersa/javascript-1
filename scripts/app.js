418e800c0c657bd35d3f7835e888513320272e90
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2024-01-27 03:09:43 +0300

    112 - FormData API

--- scripts/app.js
+++ scripts/app.js
@@ -86,6 +86,13 @@ function rerender(activeHabbitId) {
 	rerenderContent(activeHabbit);
 }
 
+/* work with days */
+function addDays(event) {
+	event.preventDefault();
+	const data = new FormData(event.target);
+	console.log(data.get('comment'));
+}
+
 /* init */
 (() => {
 	loadData();
